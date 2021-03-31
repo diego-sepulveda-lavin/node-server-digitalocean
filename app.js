b310d4851a4f7c18639665e6d2641a180e7d2a64
Diego Sepulveda <diegosl@hotmail.cl>
2021-03-31 20:38:34 -0300

    prueba AWS

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.json({msg:'Probando el micrófono'});
 });
 
+app.get("/api", (req,res) => {
+  res.json({msg:'Otra prueba fea'})
+})
+
 app.listen(port, () => {
   console.log("Up and running");
 });
